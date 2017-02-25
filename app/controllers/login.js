e722073d8bf1c915586f88f1503533992c389e6c
Shraddhak <Shraddhak@corp.rpqb.com>
2017-02-25 11:32:05 +0530

    upadate

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,5 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+     actions: {
+       
+        home: function() {
+            this.transitionToRoute('home');
+        }
+
+    }
+
 });
 
